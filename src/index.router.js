721c6c3d00d2f95fb8e03d2b6dd1193c2e7d18e9
qutibaalqasim <qalqasim9@gmail.com>
2025-02-12 14:34:06 +0300

    welcome page

--- src/index.router.js
+++ src/index.router.js
@@ -5,13 +5,16 @@ import blogRouter from './modules/blog/blog.router.js';
 import cors from 'cors';
 const initApp = (app)=>{
     connectDB();
+    app.get('/', (req,res)=>{
+        res.status(200).json({message:"Welcome"});
+    });
 app.use(cors());
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/blog', blogRouter);
 
 app.get('*', (req,res)=>{
-    return res.status(404).json({message:"page not found"})
+    return res.status(404).json({message:"page not found"});
 });
 
 app.use( (err,req,res,next)=>{
