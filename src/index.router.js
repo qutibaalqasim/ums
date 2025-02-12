95114b4f28bc4feb73ecf5997d8c6c1d51c29e46
qutibaalqasim <qalqasim9@gmail.com>
2025-02-12 14:28:46 +0300

    not found index router

--- src/index.router.js
+++ src/index.router.js
@@ -10,6 +10,10 @@ app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/blog', blogRouter);
 
+app.get('*', (req,res)=>{
+    return res.status(404).json({message:"page not found"})
+});
+
 app.use( (err,req,res,next)=>{
     return res.status(err.statusCode).json({message:err.message});
 });
